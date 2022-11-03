7e4eb66266c11f29f0eefabc1d883c28d5be8a48
komuro4182 <komuro4182@gmail.com>
2022-11-03 18:09:53 +0900

    犬表示完成

--- DogImageApp/ViewController.swift
+++ DogImageApp/ViewController.swift
@@ -1,19 +1,43 @@
-//
-//  ViewController.swift
-//  DogImageApp
-//
-//  Created by 小室　亮太 on 2022/11/02.
-//
 
 import UIKit
+import Alamofire
+import Kingfisher
+
+struct dogApiResponce: Codable {
+    let message: String
+    let status: String
+}
+
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dogImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        getDogPhotos()
     }
+    
+    @IBAction func reloadDogs(_ sender: Any) {
+        getDogPhotos()
+    }
+    func getDogPhotos() {
 
+            AF.request(
+                Constants.shared.dogUrl
+            ).responseDecodable(of:dogApiResponce.self) { response in
 
+                switch response.result {
+                case .success(let value):
+                    // print(value.message)
+                    // print(value.status)
+                    let photoUrl = URL(string: value.message)!
+                    self.dogImageView.kf.setImage(with: photoUrl)
+
+                case .failure(let error):
+                    print("error", error)
+                }
+            }
+        }
 }
-
